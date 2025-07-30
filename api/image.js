465be5ad6b9b3360503ce48876a70d574bdf38e9
Irfan Vigma Taufik <davigmacode@gmail.com>
2025-07-30 13:34:31 +0700

    Support image cache

--- api/image.js
+++ api/image.js
@@ -1,24 +1,27 @@
-export const config = {
-  runtime: 'edge',
-};
+export const config = { runtime: 'edge' };
 
 export default async function handler(req) {
   const { searchParams } = new URL(req.url);
   const url = searchParams.get("url");
 
   if (!url) {
-    return new Response('Missing ?url=', { status: 400 });
+    return new Response("Missing ?url=", { status: 400 });
   }
 
   try {
-    const imgRes = await fetch(url);
+    const response = await fetch(decodeURIComponent(url));
+
+    if (!response.ok) {
+      return new Response("Failed to fetch image", { status: response.status });
+    }
+
     const headers = new Headers({
-      'Content-Type': imgRes.headers.get("content-type") || "image/jpeg",
-      'Cache-Control': 'public, max-age=3600',
+      "Content-Type": response.headers.get("content-type") ?? "image/jpeg",
+      "Cache-Control": "public, max-age=86400, stale-while-revalidate=604800", // 1 day + revalidate 1 week
     });
 
-    return new Response(imgRes.body, { headers, status: imgRes.status});
-  } catch (e) {
-    return new Response("Failed to fetch image", { status: 500 });
+    return new Response(response.body, { headers });
+  } catch (err) {
+    return new Response("Image proxy failed", { status: 500 });
   }
 }
